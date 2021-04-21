5e173aa372edd293aa211d4ce150545895b40c34
abc <sp9512@naver.com>
2021-04-21 13:47:06 +0900

    a

--- version_py.py
+++ version_py.py
@@ -2,35 +2,40 @@ import json
 from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XML, TYPE_MHTML
 import argparse
 import time
+import os
 
 parser = argparse.ArgumentParser("VBA 매크로 PATH")
 parser.add_argument('--check_file', required=True, help="악성매크로를 확인할 파일입력")
 args = parser.parse_args()
 check_file = args.check_file
 
-# check_file = 'clean.docm'
 filedata = open(check_file, 'rb').read()
 vbaparser = VBA_Parser(check_file, data=filedata)
 results = vbaparser.analyze_macros()
 
 if vbaparser.detect_vba_macros():
-#     print('Filename    :', filename)
-#     print('OLE stream  :', stream_path)
-#     print('VBA filename:', vba_filename)
+
 
     if not results:
         print("No VBA macros found")
     else:
         with open('blacklist.json', 'r', encoding='utf-8') as j:
                 black_list = json.load(j)
-                # print(json.dumps(black_list, indent="\t"))
+
 
         for(filename, stream_path, vba_filename, vba_code) in vbaparser.extract_macros():
             for key in black_list.keys():
                 if key in vba_code.lower():
                     print(black_list[key])
-                    print(1111111111111111111111111111111111111111111111111)
-                    time.sleep(60)
+                    expaln = black_list[key].replace(" ","_")
+                    
+                    print(expaln)
+                    
+                    #os.system('python C:\\Users\hsj80\Desktop\python\Guitest.py --expaln abc --test cba')
+                    #os.system('python C:\\Users\hsj80\Desktop\python\Guitest.py --expaln {} --test {}'.format('abc','cba'))
+                    #os.system('python C:\\Users\hsj80\Desktop\python\Guitest.py --explan {} --test cba'.format(expaln))
+                    os.system('python C:\\Users\hsj80\Desktop\python\Guitest.py --explan {}'.format(expaln))
+                    #time.sleep(60)
                     break
 else:
     print("No VBA macros found")
