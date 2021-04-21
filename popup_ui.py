662f96ae4c1f7ab82d027d1a61d7e608cda76702
Suye0n <kss0908k@gmail.com>
2021-04-21 15:02:55 +0900

    ui edit

--- popup_ui.py
+++ popup_ui.py
@@ -15,35 +15,35 @@ import argparse
 parser = argparse.ArgumentParser("VBA 매크로 PATH")
 parser.add_argument('--explan', required=True, help="악성매크로를 설명")
 args = parser.parse_args()
-explain = args.explan.replace("_"," ")
-
-
-print(explain)
-
+explain = args.explan.replace("_", " ")
 
 #explain = 'abc'
+
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(609, 211)
+        MainWindow.resize(780, 300)
         MainWindow.setMouseTracking(False)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("./icon/infinity.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("./icon/infinity.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(40, 40, 61, 61))
-        self.label.setStyleSheet("background-image: url(:/inpinity/warning (1).png);")
+        self.label.setGeometry(QtCore.QRect(60, 40, 61, 61))
+        self.label.setStyleSheet(
+            "background-image: url(:/inpinity/warning (1).png);")
         self.label.setText("")
         self.label.setPixmap(QtGui.QPixmap("./icon/warning (1).png"))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(160, 20, 261, 81))
+        self.label_2.setGeometry(QtCore.QRect(150, 50, 600, 50))
         self.label_2.setObjectName("label_2")
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(140, 100, 351, 61))
+        self.textBrowser.setGeometry(QtCore.QRect(100, 130, 600, 100))
         self.textBrowser.setObjectName("textBrowser")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -56,15 +56,21 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "INPINITY program"))
-        self.label_2.setWhatsThis(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600;\">?��?�� 매크로�?? 감�???��?��?��?��?��.</span></p></body></html>"))
-        self.label_2.setText(_translate("MainWindow", "감지"))
-        self.textBrowser.setHtml(_translate("MainWindow", 
-        "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-        "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-        "p, li { white-space: pre-wrap; }\n"
-        "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"+explain+"<p></body></html>"))
+        self.label_2.setWhatsThis(_translate(
+            "MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600;\">?��?�� 매크로�?? 감�???��?��?��?��?��.</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "악성코드가 감지되었습니다."))
+        self.label_2.setStyleSheet("color: red;")
+        font1 = self.label_2.font()
+        font1.setPointSize(20)
+        font1.setBold(True)
+        self.label_2.setFont(font1)
 
+        self.textBrowser.setHtml(_translate("MainWindow",
+                                            "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                            "p, li { white-space: pre-wrap; }\n"
+                                            "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+                                            "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"+explain+"<p></body></html>"))
 
 
 if __name__ == "__main__":
